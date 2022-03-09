af34842628b710fb5eba5f234620546c42f2eebf
Samanth Krishnappa <Samanth0627@gmail.com>
2022-03-09 15:10:59 +0530

    comment changes

--- script.js
+++ script.js
@@ -1,23 +1,20 @@
 const output = document.getElementById("word-display");
-const outputcontainer = document.querySelector(".output-container");    // Not used
+// const outputcontainer = document.querySelector(".output-container");
 const input = document.querySelector(".wordinput");
 const resultElement = document.querySelector(".result");
 
-let interval = undefined; // Declaring interval globally
-
-let words = []; // Initialize display variables
+let interval = undefined;                               // Declaring interval globally
+let words = [];                                         // Initialize display variables
 let elements = [];
 let sentence = "";
 let inputfieldvalue = "";
 let number_of_words = 15;
 let word = "";
-
-let time_count = 0; // Initialize timer variables
+let time_count = 0;                                     // Initialize timer variables
 let letter_count = 0;
 let count = 0;
 let invoke = 0;
-
-let result = 0; // Initialize result variables
+let result = 0;                                         // Initialize result variables
 let correct = 0;
 let wrong = 0;
 
@@ -30,8 +27,8 @@ function setText() {
 }
 
 function pageLoad() {
-    input.focus(); // Set the cursor to the input field when the page loads / when the user hits the redo button
-    resultElement.classList.add("ouput-invisible"); // Set output to invisible
+    input.focus();                                      // Set the cursor to the input field when the page loads / when the user hits the redo button
+    resultElement.classList.add("ouput-invisible");     // Set output to invisible
 }
 
 /**
@@ -49,9 +46,7 @@ function startwatch() {
         interval = setInterval(() => {
             if (time_count < 59) {
                 time_count++;
-                //console.log(time_count)
             } else {
-                //console.log("about to stop")
                 clearInterval(interval);
             }
         }, 1000);
@@ -64,17 +59,13 @@ function startwatch() {
 input.addEventListener("keydown", (e) => {
     word = output.children[count].innerText;
 
-    /**
-     * Starts the stopwatch as soon as the first letter is typed.
-     */
+    // Starts the stopwatch as soon as the first letter is typed.
     if (count === 0 && invoke === 0) {
         startwatch();
         invoke += 1;
     }
 
-    /**
-     * We check at each keypress if the right keys are pressed or not. This is used to provide a visual cue to the typist.
-     */
+    // We check at each keypress if the right keys are pressed or not. This is used to provide a visual cue to the typist.
     if (e.code !== "Backspace" && e.code !== "Space") {
         inputfieldvalue += e.key;
     } else {
@@ -108,9 +99,7 @@ input.addEventListener("keydown", (e) => {
             input.classList.remove("input-color-change");
         }
 
-        /**
-         * On the completition of the last word, calculate the wpm.
-         */
+        // On the completition of the last word, calculate the wpm.
         if (count === number_of_words - 1) {
             resultElement.classList.remove("ouput-invisible");
             result = calculateTypingSpeed();
@@ -144,9 +133,7 @@ function calculateTypingSpeed() {
  * Reset everything to default value.
  */
 function redo() {
-    /**
-     * re-initialize global variables
-     */
+    // Re-initialize global variables
     count = 0;
     invoke = 0;
     time_count = 0;
@@ -155,10 +142,10 @@ function redo() {
     wrong = 0;
     input.value = "";
 
-    setText(); // On redo set text afresh again.
-    clearInterval(interval); // Set the timer to its initial state.
-    pageLoad(); // Set the cursor.
-    resultElement.innerText = "WPM :"; // Set the result element text
+    setText();                          // On redo set text afresh again.
+    clearInterval(interval);            // Set the timer to its initial state.
+    pageLoad();                         // Set the cursor.
+    resultElement.innerText = "WPM :";  // Set the result element text
 }
 
 /**
